235c128de55fafecad0ce12243c108d0bce98a8f
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-11 01:57:32 -0300

    feat: 🎸 add AppButton stories

--- src/stories/AppButton.stories.ts
+++ src/stories/AppButton.stories.ts
@@ -3,16 +3,23 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 import AppButton from '@/components/button/AppButton.vue'
 
 const meta: Meta<typeof AppButton> = {
-  component: AppButton
+  component: AppButton,
+  render: (args) => ({
+    components: { AppButton },
+    setup() {
+      return { args }
+    },
+    template: '<AppButton v-bind="args" />'
+  })
 }
 
 export default meta
 
-type Story = StoryObj<typeof AppButton>
+type Story = StoryObj<typeof meta>
 
 export const Basic: Story = {
-  render: () => ({
-    components: { AppButton },
-    template: '<AppButton />'
-  })
+  args: {
+    label: 'Button',
+    icon: ['fas', 'fa-user']
+  }
 }
