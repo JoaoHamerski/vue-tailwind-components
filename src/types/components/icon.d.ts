32e5cdc02a5d6116a8e561a80bba0696b95b6383
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-11 07:12:45 -0300

    fix: 🐛 compiling typescript error

--- src/types/components/icon.d.ts
+++ src/types/components/icon.d.ts
@@ -7,6 +7,6 @@ type FWIconType = 'fas' | 'far' | 'fab'
 type FWIconProp = [FWIconType, FWAvailableIcons]
 type FontAwesomeProps = InstanceType<typeof FontAwesomeIcon>['$props']
 
-export interface AppIconProps extends FontAwesomeProps {
+export interface AppIconProps extends /* @vue-ignore */ FontAwesomeProps {
   icon: FWIconProp
 }
