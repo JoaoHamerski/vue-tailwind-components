a9b105e1eb2140d219085b5f20ecdb5991f77c90
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-12 06:31:00 -0300

    feat(button): 🎸 add constants file

---
+++ src/components/button/constants.ts
@@ -0,0 +1,18 @@
+export const SIZES_CLASS = {
+  xs: {
+    btn: 'btn-xs',
+    spinner: 'loading-xs'
+  },
+  sm: {
+    btn: 'btn-sm',
+    spinner: 'loading-sm'
+  },
+  md: {
+    btn: 'btn-md',
+    spinner: 'loading-md'
+  },
+  lg: {
+    btn: 'btn-lg',
+    spinner: 'loading-lg'
+  }
+}
