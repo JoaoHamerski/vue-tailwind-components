f92e203b25de3ef20e788bd71c899d689cdf56b9
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-12 06:31:12 -0300

    feat(button): 🎸 add types declaration file

--- src/types/components/button.d.ts
+++ src/types/components/button.d.ts
@@ -1,11 +1,15 @@
 import type { AppIconProps } from '.'
+import type { SIZES_CLASS } from '@/components/button/constants'
 
 export interface AppButtonProps {
-  /** algo a mais */
   label?: string
-
   icon?: AppIconProps['icon']
   iconRight?: AppButtonProps['icon']
   loading?: boolean
   disabled?: boolean
+  rounded?: boolean
+  size: keyof typeof SIZES_CLASS
+  circle?: boolean
 }
+
+export type ButtonLabelProps = Pick<AppButtonProps, 'icon' | 'label' | 'iconRight' | 'size'>
