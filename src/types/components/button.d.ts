71ee5712d69113791b8b9a8cca9d0251de86a6ff
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-12 01:06:35 -0300

    feat(button): 🎸 update type declaration file

--- src/types/components/button.d.ts
+++ src/types/components/button.d.ts
@@ -1,6 +1,11 @@
 import type { AppIconProps } from '.'
 
 export interface AppButtonProps {
+  /** algo a mais */
   label?: string
-  icon: AppIconProps['icon']
+
+  icon?: AppIconProps['icon']
+  iconRight?: AppButtonProps['icon']
+  loading?: boolean
+  disabled?: boolean
 }
