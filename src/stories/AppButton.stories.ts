f65cd4dc0bdc8325282ada9ddc49b448b1015d9e
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-12 06:32:22 -0300

    feat(button): 🎸 add new stories

--- src/stories/AppButton.stories.ts
+++ src/stories/AppButton.stories.ts
@@ -4,6 +4,16 @@ import AppButton from '@/components/button/AppButton.vue'
 const meta: Meta<typeof AppButton> = {
   component: AppButton,
   argTypes: {
+    label: {
+      control: 'text'
+    },
+    class: {
+      control: 'text'
+    },
+    size: {
+      control: 'radio',
+      options: ['xs', 'sm', 'md', 'lg']
+    },
     icon: {
       control: 'object'
     },
@@ -15,10 +25,17 @@ const meta: Meta<typeof AppButton> = {
     },
     loading: {
       type: 'boolean'
+    },
+    rounded: {
+      type: 'boolean'
+    },
+    circle: {
+      type: 'boolean'
     }
   },
   args: {
-    label: 'Button'
+    label: 'Button',
+    size: 'md'
   },
   render: (args) => ({
     components: { AppButton },
@@ -34,6 +51,29 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
+
+export const Sizes: Story = {
+  argTypes: {
+    size: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  render: (args) => ({
+    setup() {
+      return { args }
+    },
+    template: `
+            <div class="flex gap-4">
+                <AppButton v-bind="args" size="xs" />
+                <AppButton v-bind="args" size="sm" />
+                <AppButton v-bind="args" size="md" />
+                <AppButton v-bind="args" size="lg" />
+            </div>
+        `
+  })
+}
 
 export const Icon: Story = {
   args: {
@@ -59,20 +99,93 @@ export const Loading: Story = {
   }
 }
 
-export const Accent: Story = {
+export const IconRounded: Story = {
+  argTypes: {
+    iconRight: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  args: {
+    label: undefined,
+    icon: ['fas', 'fa-user'],
+    circle: true
+  },
   render: (args) => ({
     setup() {
       return { args }
     },
     template: `
-        <div class="flex gap-2">
-            <AppButton v-bind="args" label="Default" />
-            <AppButton v-bind="args" label="Primary" class="btn-primary" />
-            <AppButton v-bind="args" label="Secondary" class="btn-secondary" />
-            <AppButton v-bind="args" label="Info" class="btn-info"/>
-            <AppButton v-bind="args" label="Success" class="btn-success" />
-            <AppButton v-bind="args" label="Warning" class="btn-warning" />
-            <AppButton v-bind="args" label="Error" class="btn-error" />
+        <AppButton v-bind="args" />
+    `
+  })
+}
+
+export const IconRoundedSizes: Story = {
+  args: IconRounded.args,
+  argTypes: {
+    size: {
+      table: {
+        disable: true
+      }
+    },
+    iconRight: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  render: (args) => ({
+    setup() {
+      return { args }
+    },
+    template: `
+            <div class="flex gap-4">
+                <AppButton v-bind="args" size="xs" />
+                <AppButton v-bind="args" size="sm" />
+                <AppButton v-bind="args" size="md" />
+                <AppButton v-bind="args" size="lg" />
+            </div>
+        `
+  })
+}
+
+export const Accent: Story = {
+  argTypes: {
+    class: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  render: (args) => ({
+    setup() {
+      return { args }
+    },
+    template: `
+        <div class="flex flex-col gap-2">
+            <div>Default</div>
+                <div class="flex gap-2">
+                <AppButton v-bind="args" label="Default" />
+                <AppButton v-bind="args" label="Primary" class="btn-primary" />
+                <AppButton v-bind="args" label="Secondary" class="btn-secondary" />
+                <AppButton v-bind="args" label="Info" class="btn-info"/>
+                <AppButton v-bind="args" label="Success" class="btn-success" />
+                <AppButton v-bind="args" label="Warning" class="btn-warning" />
+                <AppButton v-bind="args" label="Error" class="btn-error" />
+            </div>
+
+            <div>Outline</div>
+            <div class="flex gap-2">
+                <AppButton v-bind="args" label="Default" class="btn-outline" />
+                <AppButton v-bind="args" label="Primary" class="btn-primary btn-outline" />
+                <AppButton v-bind="args" label="Secondary" class="btn-secondary btn-outline" />
+                <AppButton v-bind="args" label="Info" class="btn-info btn-outline"/>
+                <AppButton v-bind="args" label="Success" class="btn-success btn-outline" />
+                <AppButton v-bind="args" label="Warning" class="btn-warning btn-outline" />
+                <AppButton v-bind="args" label="Error" class="btn-error btn-outline" />
+            </div>
         </div>
     `
   })
