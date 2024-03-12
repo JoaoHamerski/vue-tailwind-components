2fb645c4ebc5db2614c029973b3f1f222430f46e
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-12 06:29:53 -0300

    feat(button): improve component

---
+++ src/components/button/ButtonLabel.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { ButtonLabelProps } from '@/types/components'
+
+defineProps<ButtonLabelProps>()
+</script>
+
+<template>
+  <AppIcon v-if="icon" :icon="icon" fixed-width :size="size" />
+  <span v-if="label">{{ label }}</span>
+  <AppIcon v-if="iconRight" :icon="iconRight" fixed-width :size="size" />
+</template>
