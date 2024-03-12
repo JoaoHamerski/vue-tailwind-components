cade4f297f4bd8e0ac48269d1ad8c833e1b75f70
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-12 06:29:53 -0300

    feat(button): 🎸 improve component

--- src/components/button/AppButton.vue
+++ src/components/button/AppButton.vue
@@ -1,24 +1,43 @@
 <script setup lang="ts">
-import type { AppButtonProps } from '@/types/components'
 import { computed } from 'vue'
+import type { AppButtonProps } from '@/types/components'
+import { SIZES_CLASS } from './constants'
 
 import ButtonSpinner from './ButtonSpinner.vue'
+import ButtonLabel from './ButtonLabel.vue'
 
 const props = withDefaults(defineProps<AppButtonProps>(), {
   loading: false,
-  disabled: false
+  disabled: false,
+  rounded: false,
+  circle: false,
+  size: 'md'
 })
 
 const isDisabled = computed(() => props.loading || props.disabled)
+
+const sizesClass = computed(() => SIZES_CLASS[props.size])
+
+const classes = computed(() => [
+  sizesClass.value.btn,
+  {
+    'rounded-full': props.rounded,
+    'btn-circle': props.circle
+  }
+])
 </script>
 
 <template>
-  <button class="btn" :disabled="isDisabled || undefined">
-    <ButtonSpinner v-if="loading" />
-    <template v-else>
-      <AppIcon v-if="icon" :icon="icon" fixed-width />
-      <span>{{ label }}</span>
-      <AppIcon v-if="iconRight" :icon="iconRight" fixed-width />
-    </template>
+  <button class="btn" :class="classes" :disabled="isDisabled || undefined">
+    <ButtonSpinner v-if="loading" :class="sizesClass.spinner" />
+    <ButtonLabel
+      v-else
+      v-bind="{
+        icon,
+        iconRight,
+        label,
+        size
+      }"
+    />
   </button>
 </template>
