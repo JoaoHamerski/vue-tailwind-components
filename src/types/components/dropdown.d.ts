1cc5e69fce8a0ebe474f2e6b2e2790fac2494fef
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-16 08:05:28 -0300

    feat (dropdown): add type declaration file

---
+++ src/types/components/dropdown.d.ts
@@ -0,0 +1,22 @@
+import type { DROPDOWN_ALIGN_CLASS } from '@/components/dropdown/constants'
+import type { Component } from 'vue'
+
+type DropdownAlignOption = keyof typeof DROPDOWN_ALIGN_CLASS
+
+export interface AppDropdownProps {
+  label?: string
+  labelClass?: string
+  align?: DropdownAlignOption
+  options?: DropdownOption[]
+  optionsAs?: Component | string
+}
+
+export type DropdownTriggerProps = Pick<AppDropdownProps, 'label' | 'labelClass'>
+export type DropdownOptionsProps = Pick<AppDropdownProps, 'options' | 'optionsAs' | 'align'>
+
+export type DropdownOption = {
+  id?: string | number
+  label: string
+  icon?: string
+  separator?: boolean
+}
