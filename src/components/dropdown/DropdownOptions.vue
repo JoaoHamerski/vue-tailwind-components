7da479183207b5f0d4842890e24e39944e638b76
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-16 08:05:13 -0300

    feat(dropdown): add component and its children

---
+++ src/components/dropdown/DropdownOptions.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { MenuItem, MenuItems } from '@headlessui/vue'
+import type { DropdownOptionsProps } from '@/types/components'
+import { computed } from 'vue'
+import { DROPDOWN_ALIGN_CLASS, DROPDOWN_TRANSITION_CLASS } from './constants'
+
+const props = defineProps<DropdownOptionsProps>()
+
+const transitionClass = computed(() => DROPDOWN_TRANSITION_CLASS[props.align!])
+const hasOptions = computed(() => !!props.options?.length)
+
+const transitions = computed(() => ({
+  enterActiveClass: 'transition-[opacity,transform] duration-150',
+  enterFromClass: `${transitionClass.value.from} opacity-0`,
+  enterToClass: `${transitionClass.value.to} opacity-100`,
+  leaveActiveClass: 'transition-[opacity,transform] duration-150',
+  leaveToClass: `${transitionClass.value.from} opacity-0`,
+  leaveFromClass: `${transitionClass.value.to} opacity-100`
+}))
+</script>
+
+<template>
+  <Transition v-bind="transitions">
+    <MenuItems
+      class="dropdown-content absolute z-[1] menu p-2 shadow bg-base-100 rounded-box min-w-52"
+      as="ul"
+    >
+      <template v-if="hasOptions">
+        <MenuItem
+          v-for="(option, index) in options"
+          v-slot="{ active, close }"
+          :key="option.id || index"
+          as="li"
+        >
+          <Component
+            :is="optionsAs"
+            :class="{
+              'bg-base-300': active
+            }"
+            @click.prevent="close"
+          >
+            {{ option.label }}
+          </Component>
+        </MenuItem>
+      </template>
+      <template v-else>
+        <MenuItem>
+          <slot name="content" />
+        </MenuItem>
+      </template>
+    </MenuItems>
+  </Transition>
+</template>
---
+++ src/components/dropdown/DropdownTrigger.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { MenuButton } from '@headlessui/vue'
+import type { DropdownTriggerProps } from '@/types/components'
+
+defineProps<DropdownTriggerProps>()
+</script>
+
+<template>
+  <MenuButton v-if="label" :class="labelClass" v-slot="{ open }">
+    <span>{{ label }}</span>
+    <FWIcon
+      :icon="['fas', 'fa-chevron-down']"
+      class="transition-transform"
+      :class="{
+        'rotate-180': open
+      }"
+    ></FWIcon>
+  </MenuButton>
+
+  <template v-else>
+    <MenuButton v-slot="{ open }">
+      <slot name="trigger" v-bind="{ open }" />
+    </MenuButton>
+  </template>
+</template>
