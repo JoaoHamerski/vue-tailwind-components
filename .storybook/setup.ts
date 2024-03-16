7f0b85aa661fdd44142b968ea4bd9d3d563325b2
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-16 08:03:10 -0300

    feat: register storybook component helpers as global

--- .storybook/setup.ts
+++ .storybook/setup.ts
@@ -2,12 +2,22 @@ import { setup } from '@storybook/vue3'
 import Bootstrap from '../src/bootstrap'
 import '@/assets/main.css'
 import '@/libs/fontawesome'
+import type { App } from 'vue'
+import CentralizedComponent from './helpers/CentralizedComponent.vue'
+import ThemeProvider from './helpers/ThemeProvider.vue'
 
 setup((app) => {
   const bootstrap = new Bootstrap(app)
 
+  registerGlobalHelpers(app)
+
   // prettier-ignore
   bootstrap
-    .globalComponents()
-    .fontAwesome()
+  .globalComponents()
+  .fontAwesome()
 })
+
+const registerGlobalHelpers = (app: App) => {
+  app.component('CentralizedComponent', CentralizedComponent)
+  app.component('ThemeProvider', ThemeProvider)
+}
