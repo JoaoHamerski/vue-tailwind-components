c7dcba62efcedf6a5252656a7fdb5c23c93a1d2f
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-16 08:06:23 -0300

    feat(dropdown): add storybook file

---
+++ src/stories/AppDropdown.stories.ts
@@ -0,0 +1,93 @@
+import AppDropdown from '@/components/dropdown/AppDropdown.vue'
+
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { DROPDOWN_ALIGN_CLASS } from '@/components/dropdown/constants'
+
+const meta: Meta<typeof AppDropdown> = {
+  component: AppDropdown,
+  argTypes: {
+    label: {
+      control: 'text'
+    },
+    labelClass: {
+      control: 'text'
+    },
+    align: {
+      control: 'radio',
+      options: Object.keys(DROPDOWN_ALIGN_CLASS)
+    },
+    options: {
+      control: 'object'
+    }
+  },
+  args: {
+    align: 'br'
+  },
+  render: (args) => ({
+    setup() {
+      return { args }
+    },
+    template: `
+        <CentralizedComponent>
+            <AppDropdown v-bind="args" />
+
+        </CentralizedComponent>
+    `
+  })
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    label: 'Dropdown',
+    options: [
+      { label: 'Item 01' },
+      { label: 'Item 02' },
+      { label: 'Item 03' },
+      { label: 'Item 04' }
+    ]
+  }
+}
+
+export const CustomTrigger: Story = {
+  args: { options: Default.args!.options },
+  render: (args) => ({
+    setup() {
+      return { args }
+    },
+    template: `
+            <CentralizedComponent>
+                <div class="flex gap-4 items-center">
+                    <AppDropdown v-bind="args">
+                        <template #trigger>
+                            <b :style="{color: 'red'}">Custom trigger</b>
+                        </template>
+                    </AppDropdown>
+                </div>
+            </CentralizedComponent>
+        `
+  })
+}
+
+export const TriggerOpenSlotProp: Story = {
+  args: CustomTrigger.args,
+  render: (args) => ({
+    setup() {
+      return { args }
+    },
+    template: `
+            <CentralizedComponent>
+                <div class="flex gap-4 items-center">
+                    <AppDropdown v-bind="args">
+                        <template #trigger="{ open }">
+                            <b>Custom Trigger - dropdown is open: {{ open }}</b>
+                        </template>
+                    </AppDropdown>
+                </div>
+            </CentralizedComponent>
+        `
+  })
+}
