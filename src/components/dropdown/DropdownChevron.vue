e6a9a4ba7c3456d9c343ea3cc27a5bce93dc4948
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-17 03:38:03 -0300

    refactor(dropdown): 💡 add separate component for dropdown chevron icon

---
+++ src/components/dropdown/DropdownChevron.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { DropdownChevronProps } from '@/types/components'
+
+defineProps<DropdownChevronProps>()
+</script>
+
+<template>
+  <FWIcon
+    :icon="['fas', 'fa-chevron-down']"
+    class="transition-transform"
+    :class="{
+      'rotate-180': open
+    }"
+  />
+</template>
