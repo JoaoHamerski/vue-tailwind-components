9d0213b351ec6f1e69e03ffaf02c915bfda80763
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-19 06:32:20 -0300

    feat(modal): add component

---
+++ src/components/modal/ModalBackdrop.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { TransitionChild } from '@headlessui/vue'
+</script>
+
+<template>
+  <TransitionChild
+    class="fixed inset-0 z-[100] bg-black/20 backdrop-blur-sm"
+    as="div"
+    enter="transition-all"
+    enter-from="backdrop-opacity-0 opacity-0"
+    enter-to="backdrop-opacity-100 opacity-100"
+    leave="transition-all"
+    leave-from="backdrop-opacity-100 opacity-100"
+    leave-to="backdrop-opacity-0 opacity-0"
+  />
+</template>
---
+++ src/components/modal/constants.ts
@@ -0,0 +1,6 @@
+export const MODAL_SIZES_CLASS = {
+  sm: 'w-[20rem]',
+  md: 'w-[32rem]',
+  lg: 'w-[40rem]',
+  xl: 'w-[50rem]'
+}
